5c3e216b9aad94583122cd840b6e34894706d9ef
Héloïse Blin-Dray <h.blindray@gmail.com>
2025-06-23 12:23:03 +0200

    Init React TS new project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {},
+};
